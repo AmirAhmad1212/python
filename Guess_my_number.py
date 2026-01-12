abc4eb5d71e399d5cb08ed5630388b6acdc8010e
Amir-Ahmad-Coder <amir.ahmad2062@gmail.com>
2026-01-12 16:02:10 +0500

    removed some lines

--- Guess_my_number.py
+++ Guess_my_number.py
@@ -12,9 +12,3 @@ def Guess(number):
         elif user_number < number:
             print("your number is smaller than mine")
 
-        else:
-            print("you Guessed it right")
-            break
-
-
-Guess(20)
