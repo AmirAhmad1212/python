d6817efa9563eebf75d0a3ad0e4a4bfea704db9b
Amir-Ahmad-Coder <amir.ahmad2062@gmail.com>
2026-01-12 19:23:47 +0500

    python crash course chapter 2

---
+++ chapter_2/personal_massage_2.py
@@ -0,0 +1,27 @@
+#2.3 varible storing a person's name and msg
+
+varible = 'amir ahmad'
+print (f"Hello {varible} would you like to learn some python today")
+
+
+# 2.4 printing a persons name in lower and higher and title cases
+
+new_vaible = 'Amir ahmad'
+print(new_vaible.title())
+print(new_vaible.lower())
+print(new_vaible.upper())
+
+#2.5 using apostropies
+
+print('Prophet Muhammad SAW once said \n\t"seek knoledge even if you have to Go China"')
+
+# 2.7 striping extra spaces 
+
+a_line = "  hello im amir ahmad  "
+print(a_line.rstrip())
+
+# 2.8 printing fav number
+
+fav_number = 7
+
+print(f"my fav num is {fav_number}")
