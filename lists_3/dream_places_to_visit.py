22a311954aebf931f941cb8a34d61a4f16c8d6f1
Amir-Ahmad-Coder <amir.ahmad2062@gmail.com>
2026-01-13 12:02:03 +0500

    excercise 3.8-310

---
+++ lists_3/dream_places_to_visit.py
@@ -0,0 +1,21 @@
+# 3.8 seeing the world excercise
+
+
+dream_places_to_visit = ['philippines','indonesia','malaysia','china']
+print(dream_places_to_visit)
+
+print(sorted(dream_places_to_visit))
+print(dream_places_to_visit)
+print(sorted(dream_places_to_visit,reverse=True))
+print(dream_places_to_visit)
+dream_places_to_visit.reverse()
+print(dream_places_to_visit)
+dream_places_to_visit.reverse()
+print(dream_places_to_visit)
+dream_places_to_visit.sort()
+print(dream_places_to_visit)
+
+
+# 3.9 using len function
+
+print(f"the length of countres i wanna visit is {len(dream_places_to_visit)}")
